00695a4987bc046af73cfb3bdac1f4d7efe9735a
Stanislav Kotykhin <stas.kotykhin@redduck.io>
2021-12-27 15:45:07 +0200

    add output for 3rd task

--- Convolution.cu
+++ Convolution.cu
@@ -1,115 +1,117 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 #include <cuda.h>
+#include <chrono>
 
 #define pi 3.14159265359
 
-void load_image(char *fname, int Nx, int Ny, float  *img) {
-  FILE *fp;
-  
-  fp=fopen(fname,"r");
-  
-  for (int i=0;i<Ny;i++){
-    for(int j=0;j<Nx;j++)
-      fscanf(fp,"%f ",&img[i*Nx+j]);
-     fscanf(fp,"\n");
+void load_image(char* fname, int Nx, int Ny, float* img) {
+  FILE* fp;
+
+  fp = fopen(fname, "r");
+
+  for (int i = 0; i < Ny; i++) {
+    for (int j = 0; j < Nx; j++)
+      fscanf(fp, "%f ", &img[i * Nx + j]);
+    fscanf(fp, "\n");
   }
-  
+
   fclose(fp);
 }
 
 
-void save_image(char *fname, int Nx, int Ny, float  *img) {
-  FILE *fp;
-  
-  fp=fopen(fname,"w");
-  
-  for (int i=0;i<Ny;i++) {
-    for(int j=0;j<Nx;j++)
-      fprintf(fp,"%10.3f ",img[i*Nx+j]);
-    fprintf(fp,"\n");
+void save_image(char* fname, int Nx, int Ny, float* img) {
+  FILE* fp;
+
+  fp = fopen(fname, "w");
+
+  for (int i = 0; i < Ny; i++) {
+    for (int j = 0; j < Nx; j++)
+      fprintf(fp, "%10.3f ", img[i * Nx + j]);
+    fprintf(fp, "\n");
   }
-  
+
   fclose(fp);
 }
 
 
-void calculate_kernel(int kernel_size, float sigma, float *kernel) {
+void calculate_kernel(int kernel_size, float sigma, float* kernel) {
 
-  int Nk2 = kernel_size*kernel_size;
-  float x,y, center;
+  int Nk2 = kernel_size * kernel_size;
+  float x, y, center;
 
-  center = (kernel_size-1)/2.0;
-  
-  for (int i = 0; i<Nk2; i++){
-    x = (float)(i%kernel_size)-center;
-    y =(float)(i/kernel_size)-center;
-    kernel[i] = -(1.0/pi*pow(sigma,4))*(1.0 - 0.5*(x*x+y*y)/(sigma*sigma))*exp(-0.5*(x*x+y*y)/(sigma*sigma));
+  center = (kernel_size - 1) / 2.0;
+
+  for (int i = 0; i < Nk2; i++) {
+    x = (float)(i % kernel_size) - center;
+    y = (float)(i / kernel_size) - center;
+    kernel[i] = -(1.0 / pi * pow(sigma, 4)) * (1.0 - 0.5 * (x * x + y * y) / (sigma * sigma)) * exp(-0.5 * (x * x + y * y) / (sigma * sigma));
   }
 
 }
 
-void conv_img_cpu(float *img, float *kernel, float *imgf, int Nx, int Ny, int kernel_size)
+void conv_img_cpu(float* img, float* kernel, float* imgf, int Nx, int Ny, int kernel_size)
 {
-  
+
   float sum = 0;
-  int center = (kernel_size -1)/2;
+  int center = (kernel_size - 1) / 2;
   int ii, jj;
-  
-  for (int i = center; i<(Ny-center); i++)
-    for (int j = center; j<(Nx-center); j++){
+
+  for (int i = center; i < (Ny - center); i++)
+    for (int j = center; j < (Nx - center); j++) {
       sum = 0;
-      for (int ki = 0; ki<kernel_size; ki++)
-	for (int kj = 0; kj<kernel_size; kj++){
-	  ii = kj + j - center;
-	  jj = ki + i - center;
-	  sum+=img[jj*Nx+ii]*kernel[ki*kernel_size + kj];
-	}
-      imgf[i*Nx +j] = sum;
+      for (int ki = 0; ki < kernel_size; ki++)
+        for (int kj = 0; kj < kernel_size; kj++) {
+          ii = kj + j - center;
+          jj = ki + i - center;
+          sum += img[jj * Nx + ii] * kernel[ki * kernel_size + kj];
+        }
+      imgf[i * Nx + j] = sum;
     }
 }
 
 
- __global__ void conv_img_gpu(float *img, float *kernel, float *imgf, int Nx, int Ny, int kernel_size)
+__global__ void conv_img_gpu(float* img, float* kernel, float* imgf, int Nx, int Ny, int kernel_size)
 {
-  
-  int tid = threadIdx.x;    
-               
-  int iy = blockIdx.x + (kernel_size - 1)/2;  
 
-  int ix = threadIdx.x + (kernel_size - 1)/2; 
+  int tid = threadIdx.x;
 
-  int idx = iy*Nx +ix;                        
- 
-  int K2 = kernel_size*kernel_size; 
-           
-  int center = (kernel_size -1)/2;		
+  int iy = blockIdx.x + (kernel_size - 1) / 2;
+
+  int ix = threadIdx.x + (kernel_size - 1) / 2;
+
+  int idx = iy * Nx + ix;
+
+  int K2 = kernel_size * kernel_size;
+
+  int center = (kernel_size - 1) / 2;
   int ii, jj;
   float sum = 0.0;
-  
-  extern __shared__ float sdata[];         
 
-  if (tid<K2)
-    sdata[tid] = kernel[tid];             
+  extern __shared__ float sdata[];
 
-  __syncthreads();	
-  
-  if (idx<Nx*Ny){
-    for (int ki = 0; ki<kernel_size; ki++)
-      for (int kj = 0; kj<kernel_size; kj++){
-	    ii = kj + ix - center;
-	    jj = ki + iy - center;
-	    sum += img[jj*Nx+ii]*sdata[ki*kernel_size + kj];
-    }
-  
+  if (tid < K2)
+    sdata[tid] = kernel[tid];
+
+  __syncthreads();
+
+  if (idx < Nx * Ny) {
+    for (int ki = 0; ki < kernel_size; ki++)
+      for (int kj = 0; kj < kernel_size; kj++) {
+        ii = kj + ix - center;
+        jj = ki + iy - center;
+        sum += img[jj * Nx + ii] * sdata[ki * kernel_size + kj];
+      }
+
     imgf[idx] = sum;
   }
 }
 
-
-
-int main(int argc, char *argv[]){
+int main(int argc, char* argv[]) {
   cudaEvent_t start, stop;
   cudaEventCreate(&start);
   cudaEventCreate(&stop);
@@ -122,8 +124,8 @@ int main(int argc, char *argv[]){
   int Nblocks, Nthreads;
 
 
-  sprintf(finput,"input.dat");
-  sprintf(foutput,"output.dat");
+  sprintf(finput, "input.dat");
+  sprintf(foutput, "output.dat");
 
   Nx = 256;
   Ny = 256;
@@ -132,51 +134,51 @@ int main(int argc, char *argv[]){
   sigma = 0.8;
 
 
-  float *img, *imgf, *kernel;
-  
-  img = (float*)malloc(Nx*Ny*sizeof(float));
-  imgf = (float*)malloc(Nx*Ny*sizeof(float));
-  kernel = (float*)malloc(kernel_size*kernel_size*sizeof(float));  
-  
-  
-  float *d_img, *d_imgf, *d_kernel;
-  
-  cudaMalloc(&d_img,Nx*Ny*sizeof(float));
-  cudaMalloc(&d_imgf,Nx*Ny*sizeof(float));
-  cudaMalloc(&d_kernel,kernel_size*kernel_size*sizeof(float));
-  
+  float* img, * imgf, * kernel;
+
+  img = (float*)malloc(Nx * Ny * sizeof(float));
+  imgf = (float*)malloc(Nx * Ny * sizeof(float));
+  kernel = (float*)malloc(kernel_size * kernel_size * sizeof(float));
+
+
+  float* d_img, * d_imgf, * d_kernel;
+
+  cudaMalloc(&d_img, Nx * Ny * sizeof(float));
+  cudaMalloc(&d_imgf, Nx * Ny * sizeof(float));
+  cudaMalloc(&d_kernel, kernel_size * kernel_size * sizeof(float));
+
   load_image(finput, Nx, Ny, img);
   calculate_kernel(kernel_size, sigma, kernel);
 
-  cudaMemcpy(d_img, img, Nx*Ny*sizeof(float),cudaMemcpyHostToDevice);
-  cudaMemcpy(d_kernel,kernel, kernel_size*kernel_size*sizeof(float),cudaMemcpyHostToDevice);
+  cudaMemcpy(d_img, img, Nx * Ny * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_kernel, kernel, kernel_size * kernel_size * sizeof(float), cudaMemcpyHostToDevice);
 
-  Nblocks = Ny - (kernel_size-1);
-  Nthreads = Nx - (kernel_size-1);
-  
-  cudaEventRecord(start);
+  Nblocks = Ny - (kernel_size - 1);
+  Nthreads = Nx - (kernel_size - 1);
+
+  auto start_time = std::chrono::high_resolution_clock::now();
   conv_img_cpu(img, kernel, imgf, Nx, Ny, kernel_size);
-  cudaEventRecord(stop);
-  cudaEventElapsedTime(&millisecondsCpu, start, stop);
+  auto end_time = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> diffCpu = end_time - start_time;
 
   printf("\n");
-  printf("Ellapsed Time (CPU): %16.10f ms\n", millisecondsCpu);
+  printf("Ellapsed Time (CPU): %16.10f ms\n", diffCpu);
   printf("\n");
 
-  cudaEventRecord(start);
-  conv_img_gpu<<<Nblocks, Nthreads, kernel_size*kernel_size*sizeof(float)>>>(d_img, d_kernel, d_imgf, Nx, Ny, kernel_size);
+  start_time = std::chrono::high_resolution_clock::now();
+  conv_img_gpu << <Nblocks, Nthreads, kernel_size* kernel_size * sizeof(float) >> > (d_img, d_kernel, d_imgf, Nx, Ny, kernel_size);
+  end_time = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> diffGpu = end_time - start_time;
   cudaDeviceSynchronize();
-  cudaEventRecord(stop);
-  cudaEventElapsedTime(&millisecondsGpu, start, stop);
-  
-  cudaMemcpy(imgf, d_imgf, Nx*Ny*sizeof(float), cudaMemcpyDeviceToHost);
+
+  cudaMemcpy(imgf, d_imgf, Nx * Ny * sizeof(float), cudaMemcpyDeviceToHost);
   save_image(foutput, Nx, Ny, imgf);
-  
+
   printf("\n");
-  printf("Ellapsed Time (GPU): %16.10f ms\n", millisecondsGpu);
+  printf("Ellapsed Time (GPU): %16.10f ms\n", diffGpu);
   printf("\n");
-  
-  
+
+
   free(img);
   free(imgf);
   free(kernel);
